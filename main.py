0257567ca4e2ecc7283a362a813a3ab5cc6458e3
Ayananshu1 <ayananshumahapatra65@gmail.com>
2023-07-28 22:02:28 +0530

    added description

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ with col2:
     content = '''Hey all, this is Ayananshu Mahapatra.This is a portfolio website of my own where I will showcase my projects.
     I am a new bie into the software industry and am interested in learning and building projects in python.
     '''
-    st.write(content)
+    st.info(content)
+
+st.text("Below are some of the projects I have built in python.You can have a look at these if you wish to.")
+
 
